59d19e07a460d5334c3c509701549eac7378c968
Lich <liich@protonmail.com>
2021-12-21 17:23:42 +0800

    Update test

--- test/client_test.cc
+++ test/client_test.cc
@@ -7,23 +7,6 @@
 int main() {
   assert("0.1" == tdscript::VERSION);
 
-  auto client = tdscript::Client(1023);
-
-  for (int i = 0; i < 99; i++) {
-    auto response = client.td_client_manager->receive(9);
-    if (response.object) {
-      auto update = std::move(response.object);
-      if (td::td_api::updateAuthorizationState::ID == update->get_id()) {
-        auto state_id = static_cast<td::td_api::updateAuthorizationState*>(update.get())->authorization_state_->get_id(); // NOLINT(cppcoreguidelines-pro-type-static-cast-downcast)
-        if (td::td_api::authorizationStateWaitTdlibParameters::ID == state_id) {
-          client.send_parameters();
-        } else if (td::td_api::authorizationStateWaitEncryptionKey::ID == state_id) {
-          client.send_request(td::td_api::make_object<td::td_api::checkDatabaseEncryptionKey>(std::getenv("TG_DB_ENCRYPTION_KEY")));
-        }
-      }
-    }
-  }
-
   return 0;
 }
 
