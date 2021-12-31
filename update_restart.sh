2b0dbee531c53bb696454ea17c2d98ae03fb8047
Lich <liich@protonmail.com>
2021-12-31 13:38:34 +0800

    Update build script

--- update_restart.sh
+++ update_restart.sh
@@ -5,6 +5,9 @@ set -ex
 git pull
 git submodule update --init
 
+export CC=clang
+export CXX=clang
+
 cmake -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=OFF -DWITH_STATIC=ON -DWITH_IPV6=ON -S . -B build-release
 
 cmake --build build-release
